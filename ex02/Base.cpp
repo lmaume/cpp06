b273e81edf24ad5d1c0de6e199d98b948eb6e28e
Lucas Maume <lmaume@k0r3p10.42mulhouse.fr>
2025-06-27 12:38:26 +0200

    better methods, no more redundant return or braces

--- ex02/Base.cpp
+++ ex02/Base.cpp
@@ -11,38 +11,22 @@ Base* Base::generate(void)
 	std::srand(std::time(0));
 	int random = rand();
 	if (random % 3 == 0)
-	{
 		return new A;
-	}
 	else if (random % 3 == 1)
-	{
 		return new B;
-	}
 	else if (random % 3 == 2)
-	{
 		return new C;
-	}
 	return NULL;
 }
 
 void	Base::identify(Base* p)
 {
 	if (dynamic_cast<A*>(p))
-	{
 		std::cout << "*Type A.\n";
-		return ;
-	}
 	else if (dynamic_cast<B*>(p))
-	{
 		std::cout << "*Type B.\n";
-		return ;
-	}
 	else if (dynamic_cast<C*>(p))
-	{
 		std::cout << "*Type C.\n";
-		return ;
-	}
-	return ;
 }
 
 void	Base::identify(Base& p)
@@ -51,22 +35,18 @@ void	Base::identify(Base& p)
 	{
 		(void)dynamic_cast<A&>(p);
 		std::cout << "&Type A.\n";
-		return ;
 	}
 	catch(const std::exception& e){}
-		try
+	try
 	{
 		(void)dynamic_cast<B&>(p);
 		std::cout << "&Type B.\n";
-		return ;
 	}
 	catch(const std::exception& e){}
-		try
+	try
 	{
 		(void)dynamic_cast<C&>(p);
 		std::cout << "&Type C.\n";
-		return ;
 	}
 	catch(const std::exception& e){}
-	return ;
 }
